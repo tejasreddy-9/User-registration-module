60d82dfd1d2b2763a1d1502cbb207d09d051d7a5
tejasreddy-9 <tejasreddy.kota@thezennialpro.com>
2025-07-29 12:37:15 +0530

    Resolved all

--- database.py
+++ database.py
@@ -1,11 +1,11 @@
 from pymongo import MongoClient
 
-MONGO_URI = "mongodb+srv://tejasreddyis9999:Teja12399@resume-scanner.gsgmxva.mongodb.net/user_registration?retryWrites=true&w=majority"
+MONGO_URI = "mongodb+srv://<username>:<Password>@resume-scanner.gsgmxva.mongodb.net/user_registration?retryWrites=true&w=majority"
 
 try:
     client = MongoClient(MONGO_URI)
     db = client["user_registration"]
     user_collection = db["users"]
-    print("✅ MongoDB connection established successfully.")
+    print("MongoDB connection established successfully.")
 except Exception as e:
-    print(f"❌ Error connecting to MongoDB: {e}")
+    print(f"Error connecting to MongoDB: {e}")
